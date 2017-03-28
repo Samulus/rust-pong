d3223139c8fe402a6dceca54f9b9a8c6e13177d3
Samuel Vargas <savarga1@asu.edu>
2017-03-27 22:30:41 -0700

    add world.rs

---
+++ src/world.rs
@@ -0,0 +1,29 @@
+/*
+ * world.rs
+ * Author: Samuel Vargas
+ * Date: 03/27/2017
+ */
+
+/* Screen */
+pub const SCREEN_X: u32 = 800;
+pub const SCREEN_Y: u32 = 600;
+
+/* Paddle */
+pub const PADDLE_P1_X: i32 = 50;
+pub const PADDLE_P1_Y: i32 = 10;
+
+pub const PADDLE_AI_X: i32 = 750;
+pub const PADDLE_AI_Y: i32 = 10;
+
+pub const PADDLE_HEIGHT: i32 = 80;
+pub const PADDLE_WIDTH: i32 = 15;
+pub const PADDLE_SPEED: u32 = 16;
+
+/* Ball */ 
+pub const BALL_X: i32 = 400;
+pub const BALL_Y: i32 = 400;
+
+pub const BALL_HEIGHT: u32 = 10;
+pub const BALL_WIDTH: u32 = 10;
+
+pub const BALL_SPEED: u32 = 4;
